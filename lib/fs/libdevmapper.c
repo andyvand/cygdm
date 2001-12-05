80866605fd27ff1d87d751c1c206690ae5fe6b63
Alasdair Kergon <agk@redhat.com>
2001-12-05 16:41:50 +0000

    Various fixes & restructure to extract common code.

--- lib/fs/libdevmapper.c
+++ lib/fs/libdevmapper.c
@@ -12,65 +12,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/param.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/uio.h>
-#include <sys/param.h>
 #include <errno.h>
 #include <linux/kdev_t.h>
+#include <linux/device-mapper.h>
 
-#define DEV_DIR "/dev/"
-
-/* FIXME Obtain from kernel header */
-#define DM_DIR "device-mapper"
-
-static char _dm_dir[PATH_MAX] = DEV_DIR DM_DIR;
+#include "libdm-targets.h"
+#include "libdm-common.h"
 
 typedef enum {
 	DIR_CREATE,
 	DIR_REMOVE
 } do_newold_t;
-
-/*
- * Library users can provide their own logging
- * function.
- */
-static void _default_log(int level, const char *file, int line,
-			 const char *f, ...)
-{
-	va_list ap;
-
-	va_start(ap, f);
-	vfprintf(stderr, f, ap);
-	va_end(ap);
-
-	fprintf(stderr, "\n");
-}
-
-static dm_log_fn _log = _default_log;
-
-void dm_log_init(dm_log_fn fn)
-{
-	_log = fn;
-}
-
-#define log(msg, x...) _log(1, __FILE__, __LINE__, msg, ## x)
-
-struct target {
-	struct target *next;
-	char *str;
-};
-
-struct dm_task {
-	int type;
-	char *dev_name;
-
-	struct target *head, *tail;
-
-	struct dm_info info;
-};
-
-#define NR_ARGS 16
 
 /*
  * Join n path components together with /'s.
@@ -100,21 +55,6 @@ static char *mkpath(int n, ...)
 	return r;
 }
 
-struct dm_task *dm_task_create(int type)
-{
-	struct dm_task *dmt = malloc(sizeof(*dmt));
-
-	if (!dmt) {
-		log("dm_task_create: malloc(%d) failed", sizeof(*dmt));
-		return NULL;
-	}
-
-	memset(dmt, 0, sizeof(*dmt));
-
-	dmt->type = type;
-	return dmt;
-}
-
 void dm_task_destroy(struct dm_task *dmt)
 {
 	struct target *t, *n;
@@ -123,17 +63,11 @@ void dm_task_destroy(struct dm_task *dmt)
 		n = t->next;
 		free(t);
 	}
+
 	if (dmt->dev_name)
 		free(dmt->dev_name);
+
 	free(dmt);
-}
-
-int dm_task_set_name(struct dm_task *dmt, const char *name)
-{
-	if (dmt->dev_name)
-		free(dmt->dev_name);
-
-	return (dmt->dev_name = strdup(name)) ? 1 : 0;
 }
 
 int dm_task_get_info(struct dm_task *dmt, struct dm_info *info)
@@ -142,8 +76,8 @@ int dm_task_get_info(struct dm_task *dmt, struct dm_info *info)
 	return 1;
 }
 
-static struct target *_create_target(unsigned long long start,
-				     unsigned long long len,
+struct target *create_target(uint64_t start,
+				     uint64_t len,
 				     const char *type, const char *params)
 {
 	struct target *t;
@@ -154,7 +88,7 @@ static struct target *_create_target(unsigned long long start,
 
 	t = malloc(size + sizeof(struct target));
 	if (!t) {
-		log("_create_target: malloc(%d) failed",
+		log("create_target: malloc(%d) failed",
 		    size + sizeof(struct target));
 		return NULL;
 	}
@@ -162,89 +96,16 @@ static struct target *_create_target(unsigned long long start,
 	memset(t, 0, size + sizeof(struct target));
 	t->str = (char *) (t + 1);
 
-	ret = sprintf(t->str, "%Lu %Lu %s %s\n", start, len, type, params);
+	ret = sprintf(t->str, "%"PRIu64" %"PRIu64" %s %s\n", start, len, 
+		      type, params);
 	if (ret > size) {
 		/* This should be impossible, but check anyway */
-		log("_create_target internal error: Ran out of buffer space");
+		log("create_target internal error: Ran out of buffer space");
 		free(t);
 		return NULL;
 	}
 
 	return t;
-}
-
-int dm_task_add_target(struct dm_task *dmt,
-		       unsigned long long start,
-		       unsigned long long size,
-		       const char *ttype, const char *params)
-{
-	struct target *t = _create_target(start, size, ttype, params);
-
-	if (!t)
-		return 0;
-
-	if (!dmt->head)
-		dmt->head = dmt->tail = t;
-	else {
-		dmt->tail->next = t;
-		dmt->tail = t;
-	}
-
-	return 1;
-}
-
-static void _build_dev_path(char *buffer, size_t len, const char *dev_name)
-{
-	snprintf(buffer, len, "/dev/%s/%s", DM_DIR, dev_name);
-}
-
-static int _add_dev_node(const char *dev_name, dev_t dev)
-{
-	char path[PATH_MAX];
-	struct stat info;
-
-	_build_dev_path(path, sizeof(path), dev_name);
-
-	if (stat(path, &info) >= 0) {
-		if (!S_ISBLK(info.st_mode)) {
-			log("A non-block device file at '%s' "
-			    "is already present", path);
-			return 0;
-		}
-
-		if (info.st_rdev == dev)
-			return 1;
-
-		if (unlink(path) < 0) {
-			log("Unable to unlink device node for '%s'", dev_name);
-			return 0;
-		}
-	}
-
-	if (mknod(path, S_IFBLK | S_IRUSR | S_IWUSR | S_IRGRP, dev) < 0) {
-		log("Unable to make device node for '%s'", dev_name);
-		return 0;
-	}
-
-	return 1;
-}
-
-static int _rm_dev_node(const char *dev_name)
-{
-	char path[PATH_MAX];
-	struct stat info;
-
-	_build_dev_path(path, sizeof(path), dev_name);
-
-	if (stat(path, &info) < 0)
-		return 1;
-
-	if (unlink(path) < 0) {
-		log("Unable to unlink device node for '%s'", dev_name);
-		return 0;
-	}
-
-	return 1;
 }
 
 static int do_suspend(char *mnt, char *name, int on)
@@ -474,7 +335,7 @@ int dm_task_run(struct dm_task *dmt)
 		    !do_error_check(mnt, dmt->dev_name) ||
 		    !do_info(mnt, dmt->dev_name, &dmt->info))
 			return 0;
-		_add_dev_node(dmt->dev_name,
+		add_dev_node(dmt->dev_name,
 			      MKDEV(dmt->info.major, dmt->info.minor));
 		break;
 
@@ -487,7 +348,7 @@ int dm_task_run(struct dm_task *dmt)
 		if (!do_newold(mnt, dmt->dev_name, DIR_REMOVE) ||
 		    !do_info(mnt, dmt->dev_name, &dmt->info))
 			return 0;
-		_rm_dev_node(dmt->dev_name);
+		rm_dev_node(dmt->dev_name);
 		break;
 
 	case DM_DEVICE_SUSPEND:
@@ -513,13 +374,3 @@ int dm_task_run(struct dm_task *dmt)
 	return 1;
 }
 
-int dm_set_dev_dir(const char *dir)
-{
-	snprintf(_dm_dir, sizeof(_dm_dir), "%s%s", dir, DM_DIR);
-	return 1;
-}
-
-const char *dm_dir(void)
-{
-	return _dm_dir;
-}
---
+++ lib/fs/libdm-targets.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2001 Sistina Software (UK) Limited.
+ *
+ * This file is released under the LGPL.
+ */
+
+struct target {
+	char *str;
+	struct target *next;
+};
+
+struct dm_task {
+	int type;
+	char *dev_name;
+
+	struct target *head, *tail;
+
+	struct dm_info info;
+};
+
