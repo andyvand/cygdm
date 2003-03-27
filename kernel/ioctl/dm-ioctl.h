9f4ea03309bccf0fedc3d5accb2b3a0497f56103
Alasdair Kergon <agk@redhat.com>
2003-03-27 16:29:41 +0000

    Update version

--- kernel/ioctl/dm-ioctl.h
+++ kernel/ioctl/dm-ioctl.h
@@ -132,7 +132,7 @@ enum {
 #define DM_VERSION_MAJOR	1
 #define DM_VERSION_MINOR	0
 #define DM_VERSION_PATCHLEVEL	10
-#define DM_VERSION_EXTRA	"-ioctl (2003-03-26)"
+#define DM_VERSION_EXTRA	"-ioctl-cvs (2003-03-26)"
 
 /* Status bits */
 #define DM_READONLY_FLAG	0x00000001
